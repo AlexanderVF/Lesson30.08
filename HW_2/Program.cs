0debc7b9bad5a990efcb2d16241aa8086ecd2323
«alexander <favorite@bk.ru>
2022-08-31 21:24:33 +0300

    hw2

---
+++ HW_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+int x = a;
+int i = 0;
+int sum = 0;
+int n = 0;
+
+if (a < 0)
+    {
+        a = (a * (-1));
+    }
+
+while (a > 0)
+    { 
+    
+        n = a % 10;
+        a = a / 10;
+        sum = sum + n;
+        i++;
+    }
+    
+Console.WriteLine("Сумма цифр в числе " + x + " -> " + sum);
+
+
