29bd61453a32204c00fba68df655dc3a3d2d92f6
«alexander <favorite@bk.ru>
2022-09-01 20:08:05 +0300

    hw3

---
+++ HW_3/Program.cs
@@ -0,0 +1,67 @@
+﻿int[] array = new int[8];
+//string arr = Console.ReadLine();
+//int arr = int.Parse(Console.ReadLine());
+int count = array.Length;
+Console.Write(array);
+Console.Write(" -> [ ");
+
+for (int i = 0; i < count; i++)
+{
+    
+    array[i] = new Random().Next(0, 99);
+    Console.Write($"{array[i]}, ");
+}
+    
+Console.WriteLine("]");
+
+
+ 
+
+
+
+
+
+
+/* int [] arr = {1, 5, 4, 3, 2, 6, 7, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write(arr);
+    Console.Write(" -> [ ");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+   
+    Console.WriteLine("]");
+}
+
+
+PrintArray(arr); */
+
+
+
+
+
+/* int [] array = {1, 5, 4, 3, 2, 6, 7, 1};
+
+int count = array.Length;
+Console.Write(array);
+Console.Write(" -> [ ");
+
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+    
+Console.WriteLine("]"); */
+
+
+
+
+
+
+
+
+
